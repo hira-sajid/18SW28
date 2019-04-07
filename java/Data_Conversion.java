3092929352a2aa80444b27df578a19e78299e26f
hira-sajid <49371748+hira-sajid@users.noreply.github.com>
2019-04-07 13:11:57 -0700

    Add files via upload

---
+++ java/Data_Conversion.java
@@ -0,0 +1,12 @@
+//Task#2
+class Data_Conversion
+{
+	public static void main(String [] args)
+	{
+		double d=50.25;
+		long l=(long)50.25;
+		System.out.println("This is value of double ="+d);
+	System.out.println("This is conversion of double into long ="+l);
+	System.out.println("This is fractional part of double ="+(d-l));
+	}
+}
---
+++ java/MyBioData.java
@@ -0,0 +1,27 @@
+//TASK#1
+class MyBioData
+{
+	public static void main(String [] args)
+	{
+		String name="HIRA SAJJID";
+		int englishMarks=89;
+		short EEMarks=78;
+		byte BBEMarks=82;
+		long CPMarks=68;
+		float ACMarks=98.999f;
+		double TotalMarks=415.9955;
+		Float percentage=89.67f;
+		char p='%';
+		char grade='A';
+		System.out.println("My name is ="+name);
+		System.out.println(" English marks is ="+englishMarks);
+		System.out.println(" EE marks is ="+EEMarks);
+		System.out.println(" BBE marks is ="+BBEMarks);
+		System.out.println(" CP marks is ="+CPMarks);
+		System.out.println(" AC marks is ="+ACMarks);
+		System.out.println(" Total marks is ="+TotalMarks);
+		System.out.println(" percentage is ="+percentage+p);
+		System.out.println(" My grade is ="+grade);
+
+	}
+}
